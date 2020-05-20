c77414e04f4cd03554289c833500210711e6c80f
Hassan Tariq <hassan.tariq@fizz.io>
2020-05-20 11:51:31 +0500

    init create-react-library@3.0.9

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'fizz-react-ui'
+import 'fizz-react-ui/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
